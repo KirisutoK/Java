6c60ad7d195839407e0ddb1cd545c6863c43bc58
Christ Aerjil Dampog <52359975+KirisutoK@users.noreply.github.com>
2025-11-17 21:29:19 +0000

    Finished Practice

--- CH9 - Arrays/SelfStudyPractice01.java
+++ CH9 - Arrays/SelfStudyPractice01.java
@@ -10,9 +10,9 @@ public class SelfStudyPractice01 {
     public static void MovieRatingSystemArray() {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("=== Movie Rating System ===\n");
+        System.out.print("\n=== Movie Rating System ===\n");
 
-        System.out.println("How many movies do you want to rate?");
+        System.out.print("\nHow many movies do you want to rate? ");
         int ArrayMaximum = scanner.nextInt();
 
         String[] ArrayValues = new String[ArrayMaximum];
@@ -24,17 +24,37 @@ public class SelfStudyPractice01 {
             ArrayValues[i] = scanner.nextLine();
 
             System.out.print("Rate " + ArrayValues[i] + " (1-5 stars): ");
-            ArrayRating[i] = scanner.nextInt();
+            ArrayRating[i] = scanner.nextInt(); 
         }
 
         scanner.close();
 
         //==============DISPLAY==============\\
 
-        System.out.println("=== Your Movie Ratings ===");
+        System.out.print("\n=== Your Movie Ratings ===");
         for (int i = 0; i <ArrayMaximum; i++) {
-            System.out.println((i+1) + "." + ArrayValues + " - " + ArrayRating + " (" + ArrayRating + " Stars)");
+            System.out.print("\n" + (i+1) + ". " + ArrayValues[i] + " - " );
+
+            for (int j = 0; j < ArrayRating[i]; j++) {
+                System.out.print("⭐");
+            }
+            
+            System.out.print(" (" + ArrayRating[i] + " Stars)");
         }
-        System.out.println("\n");
+
+        int HighestRating = ArrayRating[0]; //Default 
+        for (int i = 0; i < ArrayMaximum; i++) { 
+            if (ArrayRating[i] > HighestRating ) { // Finds which is the highest rating
+                HighestRating = ArrayRating[i];
+            }
+        }
+
+        System.out.print("\n🏆 Highest Rated: ");
+        for (int i = 0; i < ArrayMaximum; i++) {
+            if (ArrayRating[i] == HighestRating) { // Finds the Movie of that rating and then prints out
+                System.out.print(ArrayValues[i] + " (" + ArrayRating[i] + " stars)\n");
+                break;
+            }
+        }
     }
 }
