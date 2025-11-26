c8da2cb26732743bab9fcecdb79620bf93cd3830
Christ Aerjil Dampog <52359975+KirisutoK@users.noreply.github.com>
2025-11-25 23:41:33 -0500

    why is my vscode tweaking?

---
+++ CSC-190 Object-Oriented Soft Dev/Self Study/Quiz02/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[]args) {
+        Student student1 = new Student("Christ", "177013", 65);
+
+        // setGrade(),
+        
+        student1.setGrade(150);
+        student1.displayStudentInfo();
+    }
+}
---
+++ CSC-190 Object-Oriented Soft Dev/Self Study/Quiz02/Student.java
@@ -0,0 +1,38 @@
+public class Student {
+    private String name;
+    private int grade;
+    private String studentId;
+
+    public Student(String n, int g, String studentid) {
+        this.name = n;
+        this.grade = g;
+        this.studentId = studentid;
+    }
+    //==================RETURNS=============\\
+    public String getName() { 
+        return name;
+    }
+    public int getGrades() {
+        return grade;
+    }
+    public String getStudentId() {
+        return studentId;
+    }
+    public boolean isPassing() {
+        return grade > 60;
+    }
+    //==================VOID================\\
+    public void setGrade(int g) {
+        if (g > 0 && g < 100) { // if its not 0-100
+            grade = g;
+        } else {
+            System.out.println("ERROR! Index out of bounds");
+        }
+    }
+
+    public void displayStudentInfo() {
+        System.out.println("Student: " + name);
+        System.out.println("Student ID: " + studentId);
+        System.out.println("Grade: " + grade);
+    }
+}
