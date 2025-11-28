36b37a5fa69389d3adcd25dd13cb55d43fbc51b8
Christ Aerjil Dampog <52359975+KirisutoK@users.noreply.github.com>
2025-11-28 15:07:04 -0500

    Leetcode

--- CSC-000 Self Study/Algorithms/SlidingWindow/SelfStudyFixedSlidingWindow01.java
+++ CSC-000 Self Study/Algorithms/SlidingWindow/SelfStudyFixedSlidingWindow01.java
@@ -5,15 +5,12 @@ import java.util.Arrays;
 public class SelfStudyFixedSlidingWindow01 {
     public static void main (String []args) {
         int[] Numbers = {1, 3, 4, 8, 9, 2, 5};
-<<<<<<< HEAD
-=======
 
         System.out.println(Numbers); // Array is not formatted properly so it prints something instead of [1, 3, 4, 8, 9, 2, 5]
 
         // In order to make it print like [1, 3, 4, 8, 9, 2, 5], we have to create import array:
         // import java.util.Arrays;
 
->>>>>>> f4abbdc96b152f3be6fdc54a768a9301642361dd
         int WindowSize = 3;
 
         System.out.println("Numbers: " + Arrays.toString(Numbers) + " \n"); // In order to print the whole array, it needs to be like this.
