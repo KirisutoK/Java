9e20af5e0da5a0cdbcfcea4bcba23512753b2b88
Christ Aerjil Dampog <52359975+KirisutoK@users.noreply.github.com>
2026-01-06 15:33:52 -0500

    Relearning Java OOP

--- CSC-190 Object-Oriented Soft Dev/Self Study/New (Relearning)/Lesson 1 Basic Structure/Practice05 Complex 3-Class Interaction (School Management System)/School.java
+++ CSC-190 Object-Oriented Soft Dev/Self Study/New (Relearning)/Lesson 1 Basic Structure/Practice05 Complex 3-Class Interaction (School Management System)/School.java
@@ -13,20 +13,20 @@ public class School {
 
     //==========SETTERS==========\\ NOTE: CHANGES THE VARIABLES ON THIS FILE
 
-    public void addStudent(Student student) {
+    public void addStudent(Student student) { // Adds a Student into Students Array
         boolean isEnrolled = false;
 
         //Check if the student is already in the Students[]
         for (int i = 0; i < StudentCount; i++) { // checks from 0 too how many StudentCount has
             if (Students[i].equals(student)) { // Checks if the student is already in the array
-                System.out.println(student.getStudentName() + " is already enrolled at " + SchoolName);
                 isEnrolled = true;
+                System.out.println(student.getStudentName() + " is already enrolled at " + SchoolName);
                 break; // To stop the iteration when if function is triggered to stop unnecessary memory usage
             }
         }
 
         //Triggers if it is not in the Students[]
-        if (isEnrolled == false) {
+        if (!isEnrolled) { // if it is not (false) then do this
             if (StudentCount < Students.length) { // Student Array is 10 so StudentCOunt can only reach to 9
                 Students[StudentCount] = student;
                 StudentCount++;
@@ -38,8 +38,62 @@ public class School {
     }
 
     public void addCourse(Course course) {
+        boolean isPartofCourses = false;
+
+        // Checks if the course is already in the Courses
+        for (int i = 0; i < CourseCount; i++) {
+            if (Courses[i].equals(course)) {
+                isPartofCourses = true;
+                System.out.println(course.getCourseName() + " has already been added in to " + SchoolName + " courses.");
+                break;
+            }
+        }
+
+        // Adds the course if it is not in the Courses
+        if (!isPartofCourses) {
+            // Checks if the amount of courses had reach the maximum values inside Courses array
+            if (CourseCount < Courses.length) { // If CourseCount is less than the amount of Courses.length which is 9 (Originally is 10 but readed from 0-9)
+                Courses[CourseCount] = course;
+                CourseCount++;
+                System.out.println(course.getCourseName() + " has successfully been put into the school's courses.");
+            } else {
+                System.out.println("There are no more available slots for courses");
+            }
+        }
     }
 
     //===========METHODS===========\\ NOTE: THIS ARE THE SPECIFIC PROCESS IN ORDER TO MEET THE DESIRED RESULTS
 
+    public Student findStudent(int StudentID) {
+        Student getStudent = null;
+
+        // Find if student matches
+        for (int i = 0; i < StudentCount; i++) { // Iterates the whole Students Array
+            if (Students[i].getStudentID() == (StudentID)) {
+                getStudent = Students[i];
+                break; // to stop iterating once if functions triggers
+            }
+        }
+
+        return getStudent;
+    }
+
+    public Course findCourse(String courseCode) {
+        Course getCourse = null;
+
+        for (int i = 0; i < CourseCount; i++) {
+            if (Courses[i].getCourseCode().equals(courseCode)) {
+                getCourse = Courses[i];
+                break;
+            }
+        }
+
+        return getCourse;
+    }
+
+    public void displayAllStudents() {
+        for (int i = 0; i < StudentCount; i++) {
+
+        }
+    }
 }
