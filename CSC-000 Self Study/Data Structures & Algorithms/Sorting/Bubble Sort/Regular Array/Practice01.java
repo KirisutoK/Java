6f40e05b3ba719a32433c52aa2370fc30e3c88d9
Christ Aerjil Dampog <52359975+KirisutoK@users.noreply.github.com>
2026-01-25 18:19:13 -0500

    Relearning JAVA OOP'

---
+++ CSC-000 Self Study/Data Structures & Algorithms/Sorting/Bubble Sort/Regular Array/Practice01.java
@@ -0,0 +1,23 @@
+import java.util.Arrays; // This is a regular array with multiple convenient methods. this can be such as toString()
+
+public class Practice01 {
+    public static void main(String[] args) {
+        int[] RandomNumbers = {1, 3, 8, 9, 5}; // Array
+
+        int[] SortedRandomNumbers = BubbleSort(RandomNumbers); // Calles the method
+
+        System.out.println("Original:"+Arrays.toString(RandomNumbers)); // Prints out the array
+
+
+    public static void BubbleSort(int[] array) {
+        for (int i = 0; i < array.length-1; i++) { // OUTER LOOPS
+            for (int j = 0; j < array.length-i-1; j++) { // INNER LOOPS
+                if (array[j] > array[j+1]) { // if Left > Right then
+                    int temp = array[j]; // stores the left value into a temporary variable
+                    array[j] = array[j+1]; // the left value will become the right value
+                    array[j+1] = temp; // the right value will become the temporary variable value
+                }
+            }
+        }
+    }
+}
