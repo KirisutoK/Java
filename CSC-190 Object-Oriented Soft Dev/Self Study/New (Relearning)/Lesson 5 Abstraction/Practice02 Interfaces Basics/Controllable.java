add5f635f1af77ac184d8ec5b70feaf7f3306e3b
Christ Aerjil Dampog <52359975+KirisutoK@users.noreply.github.com>
2026-02-09 10:43:15 -0500

    Relearning Java OOP

---
+++ CSC-190 Object-Oriented Soft Dev/Self Study/New (Relearning)/Lesson 5 Abstraction/Practice02 Interfaces Basics/Controllable.java
@@ -0,0 +1,7 @@
+public interface Controllable { // A Template/Contract that gives requirements for a class
+    void turnOn();
+    void turnOff();
+    boolean isOn();
+}
+
+// Methods and Interface is static by default
---
+++ CSC-190 Object-Oriented Soft Dev/Self Study/New (Relearning)/Lesson 5 Abstraction/Practice02 Interfaces Basics/Programmable.java
@@ -0,0 +1,4 @@
+public interface Programmable { // A Template/Contract that gives requirements for a class
+    void setSchedule(String schedule);
+    String getSchedule();
+}
