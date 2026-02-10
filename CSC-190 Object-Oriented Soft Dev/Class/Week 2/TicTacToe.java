7f19366a963619d8b8b07a66a5d1e6b208abab96
Christ Aerjil Dampog <52359975+KirisutoK@users.noreply.github.com>
2026-02-10 11:57:09 -0500

    Relearning Java OOP

--- CSC-190 Object-Oriented Soft Dev/Class/Week 2/TicTacToe.java
+++ CSC-190 Object-Oriented Soft Dev/Class/Week 2/TicTacToe.java
@@ -1,2 +1,28 @@
 public class TicTacToe {
+    public static void main(String[] args) {
+        char[][] TicTacToeBoard = {
+                {'x', 'o', 'x'},
+                {'x', 'x', 'o'},
+                {'x', 'o', 'o'}};
+
+        // CONDITIONS
+
+        // HORIZONTAL
+        for (int i = 0; i < TicTacToeBoard.length; i++) { // checks each row
+            for (int j = 0; j < TicTacToeBoard[i].length; j++) { // checks each index in the row
+                if (TicTacToeBoard[i][j] == 'x') {
+                    System.out.println("X is the winner");
+                } else if (TicTacToeBoard[i][j] == 'o') {
+                    System.out.println("W is the winner");
+                }
+            }
+        }
+
+        // VERTICAL
+        for (int i = 0; i < TicTacToeBoard.length; i++) { // checks -------------------->>> LEFT HERE
+
+        }
+
+        // DIAGONAL
+    }
 }
