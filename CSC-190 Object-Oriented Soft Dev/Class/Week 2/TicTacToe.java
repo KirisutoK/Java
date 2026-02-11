d523627cd5eb8c40d12e8ff25f1d9a4d5d92b337
Christ Aerjil Dampog <52359975+KirisutoK@users.noreply.github.com>
2026-02-11 12:44:58 -0500

    TicTacToe Class

--- CSC-190 Object-Oriented Soft Dev/Class/Week 2/TicTacToe.java
+++ CSC-190 Object-Oriented Soft Dev/Class/Week 2/TicTacToe.java
@@ -1,34 +1,60 @@
 public class TicTacToe {
-    public static void main(String[] args) {
-        char[][] TicTacToeBoard = {
-                {'x', 'o', 'x'},
-                {'x', 'x', 'o'},
-                {'x', 'o', 'o'}};
+    static char[][] TicTacToeBoard = { // Global Variable
+            {'o', 'x', 'x'},
+            {'x', 'x', 'x'},
+            {'x', 'o', 'o'}};
 
-        // CONDITIONS
+    public static void main(String[] args) {
+
+        // =================== CONDITIONS =================== \\
 
         // HORIZONTAL
-        for (int i = 0; i < TicTacToeBoard.length; i++) { // checks each row
-            for (int j = 0; j < TicTacToeBoard[i].length; j++) { // checks each index in the row(column)
-                if (TicTacToeBoard[i][j] == 'x') {
-                    System.out.println("X is the winner");
-                } else if (TicTacToeBoard[i][j] == 'o') {
-                    System.out.println("W is the winner");
-                }
-            }
-        }
+        HorizontalCondition();
 
         // VERTICAL
-        for (int i = 0; i < TicTacToeBoard[i].length; i++) { // checks each column
-            for (int j = 0; j < TicTacToeBoard.length; j++) { // checks each index in the column(row)
-                if (TicTacToeBoard[i][j] == 'x' ) {
-                    System.out.println("X is the winner");
-                } else if (TicTacToeBoard[i][j] == 'o') {
-                    System.out.println("W is the winner");
-                }
-            }
-        }
+        VerticalCondition();
 
         // DIAGONAL
+    }
+
+
+    // ========================= METHODS ========================= \\
+    public static void HorizontalCondition() {
+        for (int i = 0; i < TicTacToeBoard.length; i++) { // checks each row
+            if (TicTacToeBoard[i][0]=='x' && TicTacToeBoard[i][1]=='x' && TicTacToeBoard[i][2]=='x') {
+                System.out.println("X is the winner! (HORIZONTAL)");
+                break; // stops the for loop
+            } else if (TicTacToeBoard[i][0]=='o' && TicTacToeBoard[i][1]=='o' && TicTacToeBoard[i][2]=='o') {
+                System.out.println("O is the winner! (HORIZONTAL)");
+                break; // stops the for loop
+            }
+        }
+    }
+
+    public static void VerticalCondition() {
+        for (int i = 0; i < TicTacToeBoard.length; i++) { // checks each row
+            if (TicTacToeBoard[0][i]=='x' && TicTacToeBoard[1][i]=='x' && TicTacToeBoard[2][i]=='x') {
+                System.out.println("X is the winner! (VERTICAL)");
+                break; // stops the for loop
+            } else if (TicTacToeBoard[0][i]=='o' && TicTacToeBoard[1][i]=='o' && TicTacToeBoard[2][i]=='o') {
+                System.out.println("O is the winner! (VERTICAL)");
+                break; // stops the for loop
+            }
+        }
+    }
+
+    public static void DiagonalCondition() {
+        // X
+        if (TicTacToeBoard[0][0]=='x' && TicTacToeBoard[1][1]=='x' && TicTacToeBoard[2][2]=='x') {
+            System.out.println("X is the winner! (DIAGONAL)");
+        } else if (TicTacToeBoard[0][2]=='x' && TicTacToeBoard[1][1]=='x' && TicTacToeBoard[2][0]=='x') {
+            System.out.println("X is the winner! (DIAGONAL)");
+        }
+        // O
+        else if (TicTacToeBoard[0][0]=='o' && TicTacToeBoard[1][1]=='o' && TicTacToeBoard[2][2]=='o') {
+            System.out.println("O is the winner! (DIAGONAL)");
+        } else if (TicTacToeBoard[0][2]=='o' && TicTacToeBoard[1][1]=='o' && TicTacToeBoard[2][0]=='o') {
+            System.out.println("O is the winner! (DIAGONAL)");
+        }
     }
 }
