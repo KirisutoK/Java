976f52f9268562640d4194b1f4ac365d7e4dfcbe
Christ Aerjil Dampog <52359975+KirisutoK@users.noreply.github.com>
2026-02-23 23:00:01 -0500

    Relearning Java OOP

--- CSC-190 Object-Oriented Soft Dev/Self Study/New (Relearning)/Lesson 5 Abstraction/Practice02 Interfaces Basics/Main.java
+++ CSC-190 Object-Oriented Soft Dev/Self Study/New (Relearning)/Lesson 5 Abstraction/Practice02 Interfaces Basics/Main.java
@@ -48,7 +48,9 @@ public class Main {
                 ((Programmable)i).setSchedule("6:00AM - 10:00PM");
             }
         }
-        // DISPLAY SCHEDULES (PROGRAMMABLE)
+        System.out.println(" "); // Space for Readability
+
+        // DISPLAY SCHEDULES [PROGRAMMABLE]
         System.out.println("================== DISPLAY SCHEDULES [PROGRAMMABLE] ==================");
         for (SmartDevice i:SmartDevices) {
             if (i instanceof Programmable) {
@@ -57,7 +59,69 @@ public class Main {
         }
         System.out.println(" "); // Space for Readability
 
-        // DIMMABLE INTERFACES
-        // <================================================== YOU LEFT HERE!!!!!!!!!!!!!!
+        // DIMMABLE INTERFACES [SETTING UP BRIGHTNESS]
+        System.out.println("================== DIMMABLE INTERFACES (SETTING UP BRIGHTNESS) ==================");
+        for (SmartDevice i:SmartDevices) { // Setting up Brightness to 5
+            if (i instanceof Dimmable) {
+                ((Dimmable)i).setBrightness(5);
+            }
+        }
+        System.out.println(" "); // Space for Readability
+
+        // DISPLAY BRIGHTNESS [DIMMABLE]
+        System.out.println("================== DISPLAY BRIGHTNESS [DIMMABLE] ==================");
+        for (SmartDevice i:SmartDevices) {
+            if (i instanceof Dimmable) {
+                i.DisplayInformation();
+            }
+            System.out.println(" "); // Space for Readability
+        }
+        System.out.println(" "); // Space for Readability
+
+        // ADJUSTABLE INTERFACES [SETTING UP TEMPERATURE]
+        System.out.println("================== ADJUSTABLE INTERFACES (SETTING UP TEMPERATURE) ==================");
+        for (SmartDevice i:SmartDevices) { // Setting up Brightness to 5
+            if (i instanceof Adjustable) {
+                ((Adjustable)i).setTemperature(67);
+            }
+        }
+        System.out.println(" "); // Space for Readability
+
+        // DISPLAY TEMPERATURE [ADJUSTABLE]
+        System.out.println("================== DISPLAY TEMPERATURE [ADJUSTABLE] ==================");
+        for (SmartDevice i:SmartDevices) {
+            if (i instanceof Adjustable) {
+                i.DisplayInformation();
+            }
+            System.out.println(" "); // Space for Readability
+        }
+        System.out.println(" "); // Space for Readability
+
+        // DISPLAY MULTIPLE OBJECTS SHOWING ITS IMPLEMENTS
+        System.out.println("================== DISPLAY OBJECTS AND ITS IMPLEMENTATIONS ==================");
+        int interfacesCount = 0;
+        for (SmartDevice i : SmartDevices) {
+            System.out.println("Device Name: "+i.DeviceName+" ("+i.getDeviceType()+")");
+
+            if (i instanceof Controllable) {
+                System.out.println(" -> Controllable");
+                interfacesCount++;
+            }
+            if (i instanceof Programmable) {
+                System.out.println(" -> Programmable");
+                interfacesCount++;
+            }
+            if (i instanceof Dimmable) {
+                System.out.println(" -> Dimmable");
+                interfacesCount++;
+            }
+            if (i instanceof Adjustable) {
+                System.out.println(" -> Adjustable");
+                interfacesCount++;
+            }
+
+            System.out.println(" "); // Space for Readability
+        }
+        System.out.println("Total Interfaces used: "+interfacesCount);
     }
 }
