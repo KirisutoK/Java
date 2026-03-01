f03594634a544b94d9307016b076f7984dcf5fda
Christ Aerjil Dampog <52359975+KirisutoK@users.noreply.github.com>
2026-03-01 11:25:12 -0500

    Relearning Java OOP

--- CSC-190 Object-Oriented Soft Dev/Self Study/New (Relearning)/Lesson 5 Abstraction/Practice03 Advanced Interfaces & Complete System/Classes/Person/Academic/AP_Student.java
+++ CSC-190 Object-Oriented Soft Dev/Self Study/New (Relearning)/Lesson 5 Abstraction/Practice03 Advanced Interfaces & Complete System/Classes/Person/Academic/AP_Student.java
@@ -1,13 +1,15 @@
-package Classes.Person.Academic.Student;
-
-// Creation Date: February 28, 2026. at 10:37 AM
-// Last Modified: February 28, 2026. at 11:19 AM
-
+package Classes.Person.Academic.People;
 import Classes.Person.Academic.AcademicPerson;
 import Classes.Course;
+import Interfaces.Enrollable;
+import Interfaces.Gradable;
+
+// Creation Date: February 28, 2026. at 10:37 AM
+// Last Modified: March 01, 2026. at 11:19 AM
 
 public class Student
-        extends AcademicPerson {
+        extends AcademicPerson
+        implements Enrollable, Gradable {
     //=======VARIABLES=======//
     private Course[] EnrolledCourses = new Course[6];
     private double Grades;
@@ -21,11 +23,34 @@ public class Student
     //==========GETTERS==========\\ NOTE: TO ACCESS THE PRIVATE VARIABLES AND USE IT TO OTHER FILES
 
     // --- @OVERRIDE
+    // ENROLLABLE <==== [INTERFACE]
+    @Override public int getEnrolledCount() {
+        return CourseCount;
+    }
+    // TEACHABLE <==== [INTERFACE]
+    //  ACADEMIC PERSON <==== [ABSTRACT]
     @Override public String getAcademicStatus() {
-        return "YOU LEFT HERE"; // YOU LEFT HEREEEEEEEEEEEEEEEEEEEEEEEEEEEEEE!!!
+        if (Age < 23 ) {
+            return "Undergraduate";
+        }
+        return "Graduate";
+    }
+    @Override public String getRole() {
+        return "Student";
     }
 
     //==========SETTERS==========\\ NOTE: CHANGES THE VARIABLES ON THIS FILE
+
+    // --- @OVERRIDE
+    // ENROLLABLE <==== [INTERFACE]
+
+    @Override
+    public void enroll(Course course) {
+        
+    }
+
+    // TEACHABLE <==== [INTERFACE]
+    //  ACADEMIC PERSON <==== [ABSTRACT]
 
     //===========METHODS===========\\ NOTE: THIS ARE THE SPECIFIC PROCESS IN ORDER TO MEET THE DESIRED RESULTS
 }
