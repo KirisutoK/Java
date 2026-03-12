44b51c40d5cc45a40aabf93c0965cf6fd7c5c7fb
Christ Aerjil Dampog <52359975+KirisutoK@users.noreply.github.com>
2026-03-12 11:05:03 -0400

    Java Class

---
+++ CSC-190 Object-Oriented Soft Dev/Class/Week 7/ArrayListPractice/Main.java
@@ -0,0 +1,18 @@
+// Creation Date: March 12, 2026. at 10:45 AM
+// Last Modified: March 12, 2026. at 10:57 AM
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> studentRoster = new ArrayList<String>();
+        studentRoster.add("Gavin");
+        studentRoster.add("Test");
+        studentRoster.add("Run");
+
+
+        System.out.println(studentRoster);
+        studentRoster.sort(null);
+        System.out.println();
+    }
+}
