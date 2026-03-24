3968192d24192f0e2f1b131ef600b5ebf0155d24
Christ Aerjil Dampog <52359975+KirisutoK@users.noreply.github.com>
2026-03-24 10:35:11 -0400

    Relearning Java OOP

--- CSC-190 Object-Oriented Soft Dev/Self Study/Old/Practice01/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[]args) {
-        Cars cars = new Cars("Buick", 100);
-
-        cars.displayInfo(); // cars is the variable, displayInfo() is found inside cars
-    }
-}
