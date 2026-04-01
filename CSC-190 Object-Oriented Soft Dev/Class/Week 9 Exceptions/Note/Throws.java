e7a4b4daa4b96bc8b45169df875066c16d28b7cd
Christ Aerjil Dampog <52359975+KirisutoK@users.noreply.github.com>
2026-04-01 12:42:18 -0400

    Relearning Java OOP

---
+++ CSC-190 Object-Oriented Soft Dev/Class/Week 9 Exceptions/Note/Throws.java
@@ -0,0 +1,10 @@
+package Note;
+
+// Creation Date: April 01, 2026. at 12:21 PM
+// Last Modified: April 01, 2026. at 12:21 PM
+
+public class Throws {
+    public static void main(String[] args) {
+        
+    }
+}
