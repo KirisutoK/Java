31f468b56d01708fd2a78c851a4555444397ffe8
Christ Aerjil Dampog <52359975+KirisutoK@users.noreply.github.com>
2026-05-30 10:53:10 -0400

    This was by far the most confusing practice ever.

--- CSC-190 Object-Oriented Soft Dev/Self Study/Lesson 6 Packages and Access Control/Practice02 Package-Private Classes and Nested Packages/Security/Authenticator.java
+++ CSC-190 Object-Oriented Soft Dev/Self Study/Lesson 6 Packages and Access Control/Practice02 Package-Private Classes and Nested Packages/Security/Authenticator.java
@@ -1,7 +1,7 @@
 package Security;
 
 // Creation Date: May 29, 2026. at 3:32 AM
-// Last Modified: May 29, 2026. at  3:35 AM
+// Last Modified: May 30, 2026. at 10:49 AM
 
 public class Authenticator {
     //=======VARIABLES=======//
@@ -12,6 +12,9 @@ public class Authenticator {
     //==========GETTERS==========\\ NOTE: TO ACCESS THE PRIVATE VARIABLES AND USE IT TO OTHER FILES
     public static boolean authenticate(String username, String password) {
         return username.equals(password); //! I GENIUINELY DONT KNOW WHAT THIS IS.... PLEASE NEXT TIME CLAUDE COULD YOU ASK LOGICAL, REASONABLE, AND UNDERSTANDABLE ACTIVITES THAT ACTUALLY MAKE SENSE. BASED ON WHAT I UNDERSTAND, THIS METHOD DOESNT CORRELATE TO ANYTHING NOR THE RETURN IS. LIKE WHAT DO YOU MEAN IF USERNAME IS EQUALS TO PASSWORD???
+    }
+    public static String encryptData(String data) {
+        return EncryptionHelper.encrypt(data);
     }
 
     //==========SETTERS==========\\ NOTE: CHANGES THE VARIABLES ON THIS FILE
--- CSC-190 Object-Oriented Soft Dev/Self Study/Lesson 6 Packages and Access Control/Practice02 Package-Private Classes and Nested Packages/Security/EncryptionHelper.java
+++ CSC-190 Object-Oriented Soft Dev/Self Study/Lesson 6 Packages and Access Control/Practice02 Package-Private Classes and Nested Packages/Security/EncryptionHelper.java
@@ -1,7 +1,7 @@
 package Security;
 
 // Creation Date: May 29, 2026. at 3:35 AM
-// Last Modified: May 29, 2026. at  3:38 AM
+// Last Modified: May 30, 2026. at 10:46 AM
 
 class EncryptionHelper {
     //=======VARIABLES=======//
@@ -9,8 +9,23 @@ class EncryptionHelper {
     //=======CONSTRUCTOR=======// NOTE: IN ORDER TO USE THIS FILES WE NEED A CONSTRUCTOR TO CREATE INSTANCES FROM OTHER FILES
 
     //==========GETTERS==========\\ NOTE: TO ACCESS THE PRIVATE VARIABLES AND USE IT TO OTHER FILES
-    static encrypt(String data) {
-        //! WHAT?? PLEASE CHECK THE PROMPT IT LITERALLY SAYS STATIC
+    static String encrypt(String data) {
+        
+        String EncryptedString = "";
+        for (int i = data.length()-1; i >= 0; i--) { // Start from the top, ends at 0
+            EncryptedString += data.charAt(i);
+        }
+
+        return EncryptedString;
+    }
+    static String decrypt(String data) {
+
+        String DecryptedString = "";
+        for (int i = 0; i < data.length(); i++) { // Start from 0, ends at the top
+            DecryptedString += data.charAt(i);
+        }
+
+        return DecryptedString;
     }
 
     //==========SETTERS==========\\ NOTE: CHANGES THE VARIABLES ON THIS FILE
