f0c306701bad4e4d71fb32816bcb9b0edf2c4a70
Christ Aerjil Dampog <52359975+KirisutoK@users.noreply.github.com>
2026-06-17 09:14:08 -0400

    Streak

---
+++ CSC-190 Object-Oriented Soft Dev/Self Study/Lesson 8 Exception Handling/Practice03 Custom Exception/BankAccount.java
@@ -0,0 +1,17 @@
+// Creation Date: June 17, 2026. at 9:13 AM
+// Last Modified: June 17, 2026. at  9:13 AM
+
+public class BankAccount {
+    //=======VARIABLES=======//
+
+    //=======CONSTRUCTOR=======// NOTE: IN ORDER TO USE THIS FILES WE NEED A CONSTRUCTOR TO CREATE INSTANCES FROM OTHER FILES
+
+    //==========GETTERS==========\\ NOTE: TO ACCESS THE PRIVATE VARIABLES AND USE IT TO OTHER FILES
+
+    //==========SETTERS==========\\ NOTE: CHANGES THE VARIABLES ON THIS FILE
+
+    //===========METHODS===========\\ NOTE: THIS ARE THE SPECIFIC PROCESS IN ORDER TO MEET THE DESIRED RESULTS
+
+
+    // ================================================== OTHER CLASSES ================================================== \\
+}
