98d85f345dcc4ba3387c1a1eaa4b65032cb4b330
Christ Aerjil Dampog <52359975+KirisutoK@users.noreply.github.com>
2026-06-26 01:37:05 -0400

    just finished my Practice02 HashMap Basics

--- CSC-190 Object-Oriented Soft Dev/Self Study/Lesson 9 Collections Framework/Practice02 Hashmap Basics/GradeTracker.java
+++ CSC-190 Object-Oriented Soft Dev/Self Study/Lesson 9 Collections Framework/Practice02 Hashmap Basics/GradeTracker.java
@@ -1,16 +1,80 @@
 // Creation Date: June 26, 2026. at 12:55 AM
-// Last Modified: June 26, 2026. at 12:55 AM
+// Last Modified: June 26, 2026. at  1:26 AM
+
+import java.util.HashMap;
 
 public class GradeTracker {
     //=======VARIABLES=======//
+    private HashMap<String, Double> Grades;
 
     //=======CONSTRUCTOR=======// NOTE: IN ORDER TO USE THIS FILES WE NEED A CONSTRUCTOR TO CREATE INSTANCES FROM OTHER FILES
+    public GradeTracker() {
+        Grades = new HashMap<>();
+    }
 
     //==========GETTERS==========\\ NOTE: TO ACCESS THE PRIVATE VARIABLES AND USE IT TO OTHER FILES
+    public double getHighestGrade() {
+        double HighestGrade = 0;
+        for (String key:Grades.keySet()) {
+            if (Grades.get(key) > HighestGrade) {
+                HighestGrade = Grades.get(key);
+            }
+        }
+
+        return HighestGrade;
+    }
+    public double getClassAverage() {
+        double ClassAverage = 0;
+        int Count = 0;
+
+        for (String key:Grades.keySet()) {
+            ClassAverage += Grades.get(key);
+            Count++;
+        }
+
+        return ClassAverage/Count;
+    }
+    public double getGrade(String name) {
+        if (Grades.containsKey(name)) {
+            return Grades.get(name); //... this returns the value of that key
+        }
+
+        return -1.0;
+    }
+    public boolean hasStudent(String name) {
+        return Grades.containsKey(name);
+    }
 
     //==========SETTERS==========\\ NOTE: CHANGES THE VARIABLES ON THIS FILE
+    // [GRADES]
+    public void addGrade(String name, double grade) { //... would use Double as the parameter but Java has an auto-boxing feature
+        Grades.put(name, grade);
+        System.out.println("A Grade has been added!");
+    }
+    public void updateGrade(String name, double grade) {
+        if (Grades.containsKey(name)) { // if the Key exists
+            Grades.put(name, grade); // Change the value of the existing key (Overwriting it)
+        } else {
+            System.out.println(name+" is not in the tracker");
+        }
+    }
+
+    // [STUDENTS]
+    public void removeStudent(String name) {
+        if (Grades.containsKey(name)) {
+            Grades.remove(name);
+        } else {
+            System.out.println(name+" is not in the tracker!");
+        }
+    }
 
     //===========METHODS===========\\ NOTE: THIS ARE THE SPECIFIC PROCESS IN ORDER TO MEET THE DESIRED RESULTS
+    public void displayAll() {
+        System.out.println("================== DISPLAY ALL ==================");
+        for (String key: Grades.keySet()) { //... keySet() makes it so that it returns an array of keys (Data type of the key);
+            System.out.println(key+": "+Grades.get(key));
+        }
+    }
 
 
     // ================================================== OTHER CLASSES ================================================== \\
--- CSC-190 Object-Oriented Soft Dev/Self Study/Lesson 9 Collections Framework/Practice02 Hashmap Basics/Main.java
+++ CSC-190 Object-Oriented Soft Dev/Self Study/Lesson 9 Collections Framework/Practice02 Hashmap Basics/Main.java
@@ -1,8 +1,49 @@
 // Creation Date: June 26, 2026. at 12:55 AM
-// Last Modified: June 26, 2026. at 12:55 AM
+// Last Modified: June 26, 2026. at  1:36 AM
 
 public class Main {
     public static void main(String[] args) {
+        //======= CREATING OBJECTS/INSTANCES =======//
+        GradeTracker GTracker01 = new GradeTracker();
+
+        //======= ADDING Key, Values INTO THE HASHMAP  =======//
+        GTracker01.addGrade("Christ", 97.53);
+        GTracker01.addGrade("Shan", 89.67);
+        GTracker01.addGrade("Carl", 95.74);
+        GTracker01.addGrade("Sophia", 85.20);
+        GTracker01.addGrade("Bella", 81.94);
+        System.out.println();
+
+        GTracker01.displayAll();
+        System.out.println();
+
+        //======= UPDATING A HASHMAP  =======//
+        GTracker01.updateGrade("Shan", 99.67);
+        GTracker01.updateGrade("Skibidi Toilet", 67.67); // ... THIS IS TO TRIGGER IF THERE IS NO KEY PAIR FOUND IN THE HASHMAP
+        System.out.println();
+
+        //======= GETTERS FOR HASHMAP  =======//
+        System.out.println(GTracker01.getGrade("Bella"));
+        System.out.println(GTracker01.getHighestGrade());
+        System.out.println(GTracker01.getClassAverage());
+
+        //======= REMOVING A KEY IN THE HASHMAP  =======//
+        GTracker01.removeStudent("Shan");
+        System.out.println();
+
+        GTracker01.displayAll();
+        System.out.println();
 
     }
 }
+
+// Create a GradeTracker
+// Add 5 students with different grades
+// Display all grades
+// Update one student's grade
+// Try to update a student that doesn't exist
+// Get a specific student's grade
+// Display the highest scoring student
+// Display the class average
+// Remove a student
+// Display all grades again
