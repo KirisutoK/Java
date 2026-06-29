fde54365854eb0d06b49870dcabff7d0af75e45a
Christ Aerjil Dampog <52359975+KirisutoK@users.noreply.github.com>
2026-06-29 11:18:59 -0400

    just finished Problems01 but still have to wait because i have to wait to submit it to professor Claude

--- CSC-190 Object-Oriented Soft Dev/Self Study/Lesson 9 Collections Framework/Problems01 Easy (Pure Logic)/Main.java
+++ CSC-190 Object-Oriented Soft Dev/Self Study/Lesson 9 Collections Framework/Problems01 Easy (Pure Logic)/Main.java
@@ -1,8 +1,30 @@
 // Creation Date: June 28, 2026. at 4:02 AM
-// Last Modified: June 28, 2026. at  4:04 AM
+// Last Modified: June 29, 2026. at 11:17 AM
+
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
+        ArrayList<Integer> Arr01 = new ArrayList<>();
+        Arr01.add(1);
+        Arr01.add(2);
+        Arr01.add(3);
+        Arr01.add(4);
+        Arr01.add(5);
 
+        System.out.println(evenSum(Arr01));
+
+    }
+
+    public static int evenSum(ArrayList<Integer> arr) {
+        int result = 0;
+
+        for (Integer i:arr) {
+            if ((double)i%2 == 0) { // If dividing 2 leaves 0 remainder, then do this (This means that if a number is divided by 2 returns a whole number, then it's even) NOTE: in order to get that decimal point, we must convert the data type into float, long, or double in order to store decimal numbers
+                result += i;
+            }
+        }
+
+        return result;
     }
 }
