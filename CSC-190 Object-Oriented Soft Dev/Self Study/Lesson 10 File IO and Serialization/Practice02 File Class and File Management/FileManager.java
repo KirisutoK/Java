2ae2627eccd07134a28cf8e870c3575d0237c1ff
Christ Aerjil Dampog <52359975+KirisutoK@users.noreply.github.com>
2026-07-17 21:57:46 -0400

    i am tired.

---
+++ CSC-190 Object-Oriented Soft Dev/Self Study/Lesson 10 File IO and Serialization/Practice02 File Class and File Management/FileManager.java
@@ -0,0 +1,22 @@
+// Creation Date: July 17, 2026. at 9:47 PM
+// Last Modified: July 17, 2026. at  9:48 PM
+
+public class FileManager {
+    //=======VARIABLES=======//
+    private String FolderPath;
+
+    //=======CONSTRUCTOR=======// NOTE: IN ORDER TO USE THIS FILES WE NEED A CONSTRUCTOR TO CREATE INSTANCES FROM OTHER FILES
+    public FileManager(String FolderPath) {
+        this.FolderPath = FolderPath;
+    }
+
+
+    //==========GETTERS==========\\ NOTE: TO ACCESS THE PRIVATE VARIABLES AND USE IT TO OTHER FILES
+
+    //==========SETTERS==========\\ NOTE: CHANGES THE VARIABLES ON THIS FILE
+
+    //===========METHODS===========\\ NOTE: THIS ARE THE SPECIFIC PROCESS IN ORDER TO MEET THE DESIRED RESULTS
+
+
+    // ================================================== OTHER CLASSES ================================================== \\
+}
---
+++ CSC-190 Object-Oriented Soft Dev/Self Study/Lesson 10 File IO and Serialization/Practice02 File Class and File Management/Main.java
@@ -0,0 +1,13 @@
+// Creation Date: July 17, 2026. at 9:47 PM
+// Last Modified: July 17, 2026. at  9:47 PM
+
+public class Main {
+    // =========================== CLASS VARIABLES =========================== \\
+
+    // =========================== MAIN =========================== \\
+    public static void main(String[] args) {
+
+    }
+
+    // =========================== OTHER METHODS =========================== \\
+}
