7781adc8c12cd8ad15824d22d27a027a0b40dc2c
Christ Aerjil Dampog <52359975+KirisutoK@users.noreply.github.com>
2026-07-19 11:02:32 -0400

    finished Practice02

--- CSC-190 Object-Oriented Soft Dev/Self Study/Lesson 10 File IO and Serialization/Practice02 File Class and File Management/FileManager.java
+++ CSC-190 Object-Oriented Soft Dev/Self Study/Lesson 10 File IO and Serialization/Practice02 File Class and File Management/FileManager.java
@@ -1,9 +1,8 @@
 // Creation Date: July 17, 2026. at 9:47 PM
-// Last Modified: July 18, 2026. at 10:12 PM
+// Last Modified: July 19, 2026. at 11:02 AM
 
 import java.io.File;
 import java.io.IOException;
-import java.util.logging.SocketHandler;
 
 public class FileManager {
     //=======VARIABLES=======//
@@ -15,9 +14,8 @@ public class FileManager {
         if (!f.exists()) {
             f.mkdirs();
             System.out.println("Directory has been created: "+FolderPath);
-        } else {
-            WorkingDirectory = FolderPath;
         }
+        WorkingDirectory = FolderPath;
     }
 
     //==========GETTERS==========\\ NOTE: TO ACCESS THE PRIVATE VARIABLES AND USE IT TO OTHER FILES
@@ -25,13 +23,10 @@ public class FileManager {
         File Directory = new File(WorkingDirectory);
         File[] Files = Directory.listFiles();
 
+        System.out.println("+++++++++++++++++++++++++ "+Directory.getName()+" +++++++++++++++++++++++++");
         if (Files != null) { // IF THE WORKING DIRECTORY IS NOT NULL THEN
             for (File f:Files) {
-                System.out.println("Name: "+f.getName());
-                System.out.println("Size: "+f.length()+" Bytes");
-                System.out.println("File: "+f.isFile());
-                System.out.println("Directory: "+f.isDirectory());
-                System.out.println();
+                getFileInfo(f.getName());
             }
         } else {
             System.out.println("Working Directory is Null or does not exists!");
@@ -55,15 +50,16 @@ public class FileManager {
     };
 
     //==========SETTERS==========\\ NOTE: CHANGES THE VARIABLES ON THIS FILE
+    // [File]
     public void createFile(String filename) {
         File f = new File(WorkingDirectory, filename); // Paramenter(Parent, Child) or Parameter(Folder, File)
 
         try {
             if (f.exists()) {
-                System.out.println(filename+" already exist!");
+                System.out.println("[File] "+filename+" already exist!");
             } else {
                 f.createNewFile();
-                System.out.println(filename+" has been created under "+WorkingDirectory);
+                System.out.println("[File] "+filename+" has been created under "+WorkingDirectory);
             }
         } catch (IOException e) {
             System.out.println("ERROR: "+e.getMessage());
@@ -74,22 +70,62 @@ public class FileManager {
 
         if (f.exists()) {
             f.delete();
-            System.out.println(filename+" has been deleted");
+            System.out.println("[File] "+filename+" has been deleted");
         } else {
-            System.out.println(filename+" does not exists!");
+            System.out.println("[File] "+filename+" does not exists!");
         }
     }
     public void renameFile(String oldname, String newname) {
-        File oldfile = new File(oldname);
-        File newfile = new File(newname);
+        File oldfile = new File(WorkingDirectory ,oldname);
+        File newfile = new File(WorkingDirectory, newname);
 
         if (oldfile.exists()) {
-            oldfile.renameTo(newfile);
+            if (oldfile.renameTo(newfile)) {
+                System.out.println("[File] "+oldname+" has been renamed into "+newname);
+            } else {
+                System.out.println("[File] "+oldname+" can not be renamed into "+newname+" because it already exists!");
+            }
         } else {
-            System.out.println(oldname+" does not exists!");
+            System.out.println("[File] "+oldname+" does not exists!");
         }
     };
-    
+
+    // [Directory]
+    public void createDirectory(String directoryname) {
+        File Directory = new File(WorkingDirectory, directoryname);
+
+        if (Directory.exists()) {
+            System.out.println("[Directory] "+directoryname+" already exists!");
+        } else {
+            Directory.mkdir();
+            System.out.println("[Directory] "+directoryname+" has been successfully created!");
+        }
+    }
+    public void changeDirectory(String FolderPath) {
+        File oldf = new File(WorkingDirectory);
+        File f = new File(FolderPath);
+        if (!f.exists()) {
+            System.out.println("[Directory] "+f.getName()+" does not exists!");
+        } else {
+            WorkingDirectory = FolderPath;
+            System.out.println("[Directory] The working directory has successfully changed from "+oldf.getName()+" into "+f.getName());
+        }
+    }
+    public void renameDirectory(String oldname, String newname) {
+        File oldDirectory = new File(WorkingDirectory, oldname);
+        File newDirectory = new File(WorkingDirectory, newname);
+
+        if (oldDirectory.exists()) {
+            if (oldDirectory.renameTo(newDirectory)) {
+                System.out.println("[Directory] "+oldname+" has been renamed into "+newname);
+            } else {
+                System.out.println("[Directory] "+oldname+" can not be renamed into "+newname+" because it already exists!");
+            }
+        } else {
+            System.out.println("[Directory] "+oldname+" does not exists!");
+        }
+
+    };
 
     //===========METHODS===========\\ NOTE: THIS ARE THE SPECIFIC PROCESS IN ORDER TO MEET THE DESIRED RESULTS
 
--- CSC-190 Object-Oriented Soft Dev/Self Study/Lesson 10 File IO and Serialization/Practice02 File Class and File Management/Main.java
+++ CSC-190 Object-Oriented Soft Dev/Self Study/Lesson 10 File IO and Serialization/Practice02 File Class and File Management/Main.java
@@ -1,5 +1,5 @@
 // Creation Date: July 17, 2026. at 9:47 PM
-// Last Modified: July 18, 2026. at  9:37 AM
+// Last Modified: July 19, 2026. at 10:59 AM
 
 public class Main {
     // =========================== CLASS VARIABLES =========================== \\
@@ -7,12 +7,48 @@ public class Main {
     // =========================== MAIN =========================== \\
     public static void main(String[] args) {
         //======= CREATING OBJECTS =======//
-        FileManager FManager = new FileManager("C:\\Users\\kiris\\OneDrive - Finger Lakes Community College\\Documents\\FLCC\\Coding\\Java\\CSC-190 Object-Oriented Soft Dev\\Self Study\\Lesson 10 File IO and Serialization\\Practice02 File Class and File Management");
+        FileManager FManager = new FileManager("C:\\Users\\kiris\\OneDrive - Finger Lakes Community College\\Documents\\FLCC\\Coding\\Java\\CSC-190 Object-Oriented Soft Dev\\Self Study\\Lesson 10 File IO and Serialization\\Practice02 File Class and File Management\\FileSandBox");
 
         //======= METHODS =======//
-        // FManager.createFile("Test");
+        // FManager.createFile("Test.txt");
         // FManager.deleteFile("Test");
         // FManager.listFiles();
+        // FManager.renameFile("Test", "Test.txt");
+
+
+        //======= CLAUDE =======//
+        // [CREATING FILES]
+        FManager.createFile("Test01");
+        FManager.createFile("Test02");
+        FManager.createFile("Test03");
+
+        // [CREATING A DUPLICATE]
+        FManager.createFile("Test01");
+
+//        // [CREATING A DIRECTORY] <=========== THIS MY OWN PERSONAL CODE
+//        FManager.createDirectory("TestDirectory01");
+//        FManager.renameDirectory("TestDirectory01", "TestDirectory02");
+//
+//        // [CHANGING DIRECTORY]
+//        FManager.changeDirectory("C:\\Users\\kiris\\OneDrive - Finger Lakes Community College\\Documents\\FLCC\\Coding\\Java\\CSC-190 Object-Oriented Soft Dev\\Self Study\\Lesson 10 File IO and Serialization\\Practice02 File Class and File Management");
+
+        // [LIST ALL FILES IN THE DIRECTORY]
+        FManager.listFiles();
+
+        // [GET INFO IN ONE SPECIFIC FILE]
+        FManager.getFileInfo("Test01");
+
+        // [RENAME ONE FILE]
+        FManager.renameFile("Test01", "Test04");
+
+        // [LIST ALL FILES IN THE DIRECTORY]
+        FManager.listFiles();
+
+        // [DELETE A FILE]
+        FManager.deleteFile("Test04");
+
+        // [LIST ALL FILES IN THE DIRECTORY]
+        FManager.listFiles();
 
     }
 
