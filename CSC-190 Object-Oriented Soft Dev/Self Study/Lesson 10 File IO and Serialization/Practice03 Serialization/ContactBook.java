4e8cf80cd2a9b9611227701e5a7af316854e2178
Christ Aerjil Dampog <52359975+KirisutoK@users.noreply.github.com>
2026-07-21 21:54:15 -0400

    so many issues with my wifi right now

--- CSC-190 Object-Oriented Soft Dev/Self Study/Lesson 10 File IO and Serialization/Practice03 Serialization/ContactBook.java
+++ CSC-190 Object-Oriented Soft Dev/Self Study/Lesson 10 File IO and Serialization/Practice03 Serialization/ContactBook.java
@@ -1,28 +1,49 @@
 // Creation Date: July 20, 2026. at 6:41 PM
-// Last Modified: July 20, 2026. at  7:19 PM
+// Last Modified: July 21, 2026. at  8:39 PM
 
+import java.io.*;
 import java.util.ArrayList;
 
 public class ContactBook {
     //=======VARIABLES=======//
     private ArrayList<Contact> Contacts;
-    private String FileName;
+    private String FilePath;
 
     //=======CONSTRUCTOR=======// NOTE: IN ORDER TO USE THIS FILES WE NEED A CONSTRUCTOR TO CREATE INSTANCES FROM OTHER FILES
-    public ContactBook(String FileName) {
+    public ContactBook(String FilePath) {
         Contacts = new ArrayList<>();
-        this.FileName = FileName;
+        this.FilePath = FilePath;
     }
 
     //==========GETTERS==========\\ NOTE: TO ACCESS THE PRIVATE VARIABLES AND USE IT TO OTHER FILES
-
-    //! public void saveToFile() {}
+    public void saveToFile() {
+        try {
+            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(FilePath));
+            oos.writeObject(Contacts);
+            oos.close();
+            System.out.println("Contacts has been successfully saved into the File!");
+        } catch (IOException e) {
+            System.out.println("ERROR: "+e.getMessage());
+        }
+    }
+    public void findContact() {
+        for (Contact c:Contacts) {
+            c.displayInfo();
+        }
+    }
 
     //==========SETTERS==========\\ NOTE: CHANGES THE VARIABLES ON THIS FILE
     public void addContact(Contact c) {
         if (c == null) {
             System.out.println("ERROR: You can't add a null contact!");
             return; // stops the whole method here
+        }
+
+        for (Contact contact:Contacts) {
+            if ((c.getName().equals(contact.getName())) && (c.getEmail().equals(contact.getEmail())) && (c.getPhoneNumber().equals(contact.getPhoneNumber()))) {
+                System.out.println(c.getName()+" already exists!");
+                return; // stops the whole method here
+            }
         }
 
         Contacts.add(c);
@@ -43,11 +64,23 @@ public class ContactBook {
 
         System.out.println("ERROR: "+n+" does not exists!");
     }
-    //! public void loadFromFile() {}
+    public void loadFromFile() {
+        try {
+            ObjectInputStream ois = new ObjectInputStream(new FileInputStream(FilePath));
+            Contacts = (ArrayList<Contact>) ois.readObject();
+            ois.close();
+
+            System.out.println("Contacts has been successfully loaded from the File!");
+        } catch (IOException e) {
+            System.out.println("EROR: "+e.getMessage());
+        } catch (ClassNotFoundException e) {
+            System.out.println("No Saved Contact File found!");;
+        }
+    }
 
     //===========METHODS===========\\ NOTE: THIS ARE THE SPECIFIC PROCESS IN ORDER TO MEET THE DESIRED RESULTS
     public void displayAllContacts() {
-        System.out.println("++++++++++++++++++ "+FileName+" Contacts ++++++++++++++++++");
+        System.out.println("++++++++++++++++++ "+ FilePath +" Contacts ++++++++++++++++++");
         for (Contact c:Contacts) {
             c.displayInfo();
             System.out.println();
--- CSC-190 Object-Oriented Soft Dev/Self Study/Lesson 10 File IO and Serialization/Practice03 Serialization/Main.java
+++ CSC-190 Object-Oriented Soft Dev/Self Study/Lesson 10 File IO and Serialization/Practice03 Serialization/Main.java
@@ -1,12 +1,44 @@
 // Creation Date: July 20, 2026. at 6:41 PM
-// Last Modified: July 20, 2026. at  6:41 PM
+// Last Modified: July 20, 2026. at  9:25 PM
 
 public class Main {
     // =========================== CLASS VARIABLES =========================== \\
 
     // =========================== MAIN =========================== \\
     public static void main(String[] args) {
+        //======= CREATING OBJECTS/INSTANCES =======//
+        ContactBook CBook01 = new ContactBook("First Contact Book");
 
+        //======= FOR CLAUDE =======//
+        CBook01.loadFromFile(); // Should say null or error because nothing has saved up yet
+        CBook01.addContact(new Contact("Test@Gmail.com", "123456789", "Tester01"));
+        CBook01.addContact(new Contact("Test@Gmail.com", "123456789", "Tester02"));
+        CBook01.addContact(new Contact("Test@Gmail.com", "123456789", "Tester03"));
+        System.out.println();
+
+        // [DISPLAY]
+        CBook01.displayAllContacts();
+        System.out.println();
+
+        // [SAVE TO FILE]
+        CBook01.saveToFile();
+        System.out.println();
+
+        // [REMOVING A CONTACT]
+        CBook01.removeContact("Tester01");
+        System.out.println();
+
+        // [DISPLAY]
+        CBook01.displayAllContacts();
+        System.out.println();
+
+        // [LOAD FILE]
+        CBook01.loadFromFile(); // Loads up the previous saved file
+        System.out.println();
+
+        // [DISPLAY]
+        CBook01.displayAllContacts();
+        System.out.println();
     }
 
     // =========================== OTHER METHODS =========================== \\
